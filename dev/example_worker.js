00516b140dbc6c40efbbd90d366636ccbc0cfaf1
calebbabin <object.caleb@gmail.com>
2025-02-10 11:33:16 -0500

    sketches out rough ideas for the worker messaging protocol

--- dev/example_worker.js
+++ dev/example_worker.js
@@ -1,9 +1,64 @@
-
-onmessage = (event) => {
-	console.log(event.data);
+function sendType() {
+	postMessage({
+		event_name: 'type',
+		data: 'text',
+	})
 }
 
-postMessage({
-	event_name: 'hello',
-	data: 'world',
-})
+const items = [];
+
+self.addEventListener('message', (event) => {
+	console.log(event.data);
+	const [event_name, data] = event.data;
+	switch (event_name) {
+		case 'update': {
+			const id = data.id;
+			let item = items.find(item => item.id === id);
+			if (!item) {
+				items.push(data);
+				item = data;
+			} else {
+				item = { ...item, ...data };
+			}
+			const context = item.canvas.getContext('2d');
+			context.clearRect(0, 0, item.canvas.width, item.canvas.height);
+			context.fillStyle = 'red';
+
+			let textSize = 0;
+			let measuredSize;
+			const text = item.data.text;
+			while (true) {
+				textSize++;
+				context.font = '400 ' + textSize + 'px sans-serif';
+				measuredSize = context.measureText(text);
+				if (measuredSize.width >= item.canvas.width) {
+					textSize--;
+					break;
+				};
+
+				if (textSize > 100) {
+					textSize = 100;
+					break;
+				}
+			}
+
+			const textHeight = measuredSize.actualBoundingBoxAscent;
+
+			context.font = '400 ' + textSize + 'px sans-serif';
+			context.strokeStyle = 'white';
+			context.lineWidth = 2;
+			context.strokeText(text, 0, item.canvas.height / 2 + textHeight / 2);
+			context.fillText(text, 0, item.canvas.height / 2 + textHeight / 2);
+
+
+			context.lineWidth = 1;
+			context.beginPath();
+			context.rect(0, item.canvas.height / 2, item.canvas.width, 0);
+			context.stroke();
+		} break;
+		case 'type': {
+			sendType();
+		} break;
+	}
+});
+
--- dev/main.js
+++ dev/main.js
@@ -1,9 +1,31 @@
-import CanvasEditor from '../src'
+import Editor from '../src'
 import './style.css'
 
 
-const canvasInstance = new CanvasEditor({});
+const editor = new Editor({});
 const container = document.getElementById('container');
-container.appendChild(canvasInstance.container);
+container.appendChild(editor.container);
 
-canvasInstance.addWorker(new Worker(new URL('./example_worker.js', import.meta.url)));
+const worker = new Worker(new URL('./example_worker.js', import.meta.url), {
+	type: 'module'
+});
+// worker.onerror = (event) => {
+// 	console.error(event);
+// }
+editor.addWorker(worker);
+
+editor.addItem('text', {
+	text: 'Hello world!',
+	width: 200,
+	height: 200,
+	x: 100,
+	y: 100,
+	rotation: 45,
+});
+editor.addItem('text', {
+	text: 'Goodbye world...',
+	width: 200,
+	height: 200,
+	x: -100,
+	y: -100,
+});
